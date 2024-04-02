eb1e713c24f93277848ba48c5db3107cb859844c
Kim Pang <pang921008@gmail.com>
2024-04-02 12:49:42 +0800

    try to send message by producer

--- demo/src/main/java/com/kafka/demo/DemoApplication.java
+++ demo/src/main/java/com/kafka/demo/DemoApplication.java
@@ -1,5 +1,10 @@
 package com.kafka.demo;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +13,27 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		Properties properties = new Properties();
+		properties.put("acts", "1"); // 0 : not pending the response from the broker, 1 : pending the response from the broker , 2: pending the response from the leader
+		properties.put("retries", "2"); // 0 : no retry, 1 : retry once, 2 : retry twice
+		properties.put("bootstrap.servers", "localhost:9092");
+		properties.put("batch.size", 16384);
+		properties.put("linger.ms", 1000);
+		properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		// confirm exactly once 
+		// properties.put("idempotence", "true");
+		// properties.put("retries", Integer.MAX_VALUE); // 0 : no retry, 1 : retry once, 2 : retry twice
+		// properties.put("acts", "ALL");
+
+		Producer<String, String> producer = new KafkaProducer<>(properties);
+		for (int i = 0; i < 100; i++) {
+			ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", Integer.toString(i), Integer.toString(i));
+			producer.send(record);
+		}
+
+		producer.close();
 	}
 
 }
--- demo/src/test/java/com/kafka/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/kafka/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.kafka.demo;
 
+import java.util.Properties;
+
 import org.junit.jupiter.api.Test;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testProducer() {
+
 	}
 
 }
